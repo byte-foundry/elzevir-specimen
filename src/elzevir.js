afdd093ecd97cfddc8c4ff916f6e171976449fb6
yannick mathey <hello@yannickmathey.com>
2016-03-21 13:54:46 +0100

    last fixes before team checking

--- src/elzevir.js
+++ src/elzevir.js
@@ -1,21 +1,11 @@
 // Setup fonts
-Ptypo.createFont('elzevir-regular', 'elzevir').then( function() {
-	Ptypo.changeParam(90, 'thickness', 'elzevir-regular');
-	loaded('elzevir-regular');
-});
-Ptypo.createFont('elzevir-light', 'elzevir').then( function() {
-	Ptypo.changeParam(40, 'thickness', 'elzevir-light');
-	loaded('elzevir-light');
+Ptypo.createFont('elzevir-book', 'elzevir').then( function() {
+	Ptypo.changeParam(80, 'thickness', 'elzevir-book');
+	loaded('elzevir-book');
 });
 Ptypo.createFont('elzevir-bold', 'elzevir').then( function() {
 	Ptypo.changeParam(120, 'thickness', 'elzevir-bold');
 	loaded('elzevir-bold');
-});
-Ptypo.createFont('elzevir-caption', 'elzevir').then( function() {
-	Ptypo.changeParam(100, 'thickness', 'elzevir-caption');
-	Ptypo.changeParam(700, 'xHeight', 'elzevir-caption');
-	Ptypo.changeParam(1.8, 'width', 'elzevir-caption');
-	loaded('elzevir-caption');
 });
 Ptypo.createFont('elzevir-sans', 'elzevir').then( function() {
 	Ptypo.changeParam(1, 'serifWidth', 'elzevir-sans');
@@ -25,40 +15,34 @@ Ptypo.createFont('elzevir-sans', 'elzevir').then( function() {
 
 var itemLoaded = 0;
 function loaded( el ) {
-	$('#' + el + ' > span').html('<img src="img/loaded.svg" />');
+	$('#' + el + ' > span').html('<img width="25" src="img/loaded.svg" />');
 	itemLoaded++;
 
-	if( itemLoaded == 5 ){
-		$('#loading').hide();
+	if( itemLoaded == 3 ){
+		$('#loading').slideUp();
 	}
 }
 
 $( document ).on( "mousemove", function( event ) {
 	centerH = (event.pageX - $( document ).width() / 2) / $( document ).width();
-	centerV = (event.pageY - $( document ).height() / 2) / $( document ).height();
-	// console.log( "H: " + centerH + ", V: " + centerV );
+
+	var bookH = Ptypo.getParam('serifHeight', 'elzevir-book');
+	Ptypo.changeParam(15 * (1 + centerH), 'serifHeight', 'elzevir-book');
+	$('.elzevir-book > .value > .int.sh').html(bookH.toFixed(0));
+	var bookT = Ptypo.getParam('thickness', 'elzevir-book');
+	Ptypo.changeParam(80 * (1 + centerH), 'thickness', 'elzevir-book');
+	$('.elzevir-book > .value > .int.t').html(bookT.toFixed(0));
+	var bookw = Ptypo.getParam('width', 'elzevir-book');
+	Ptypo.changeParam((1 + centerH), 'width', 'elzevir-book');
+	$('.elzevir-book > .value > .int.w').html(bookw.toFixed(2));
 
 	var bold = Ptypo.getParam('thickness', 'elzevir-bold');
 	Ptypo.changeParam(120 * (1 + centerH), 'thickness', 'elzevir-bold');
-	$('.elzevir-bold > .value > .int').html(bold.toFixed(2));
-
-	var regular = Ptypo.getParam('thickness', 'elzevir-regular');
-	Ptypo.changeParam(90 * (1 + centerH), 'thickness', 'elzevir-regular');
-	$('.elzevir-regular > .value > .int').html(regular.toFixed(2));
-
-	var light = Ptypo.getParam('slant', 'elzevir-light');
-	Ptypo.changeParam(15 * (1 + centerH), 'slant', 'elzevir-light');
+	$('.elzevir-bold > .value > .int').html(bold.toFixed(0));
 
 	var sans = Ptypo.getParam('xHeight', 'elzevir-sans');
 	Ptypo.changeParam(800 * (1 + centerH), 'xHeight', 'elzevir-sans');
-	$('.elzevir-sans > .value > .int').html(sans.toFixed(2));
-
-	// var sansW = Ptypo.getParam('serifWidth', 'elzevir-sans');
-	// var sansH = Ptypo.getParam('serifHeight', 'elzevir-sans');
-	// Ptypo.changeParam(90 * (1 + centerH), 'serifWidth', 'elzevir-sans');
-	// Ptypo.changeParam(50 * (1 + centerH), 'serifHeight', 'elzevir-sans');
-	// $('.elzevir-sans > .value > .int.width').html(sansH.toFixed(2));
-	// $('.elzevir-sans > .value > .int.height').html(sansH.toFixed(2));
+	$('.elzevir-sans > .value > .int').html(sans.toFixed(0));
 });
 
 $('#zoom-in').click(function() {
@@ -68,7 +52,6 @@ $('#zoom-in').click(function() {
 $('#zoom-out').click(function() {
    updateZoom(-0.1);
 });
-
 
 zoomLevel = 1;
 
